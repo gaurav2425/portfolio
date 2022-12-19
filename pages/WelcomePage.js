af64b5b98b743bb39cdd42af2d3422a76926d188
Gaurav <gauravburande2425@gmail.com>
2022-12-19 15:38:24 +0530

    Intro Content Updated

--- pages/WelcomePage.js
+++ pages/WelcomePage.js
@@ -2,7 +2,15 @@
 import styles from "../styles/Welcomepage.module.css";
 import MyImg from "../assets/myimg.jpg";
 import Footer from "./components/Footer";
+import IconButton from "@mui/material/IconButton";
 import Navbar from "./components/Navbar";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Tooltip from "@mui/material/Tooltip";
 function WelcomePage() {
   return (
     <div>
@@ -21,6 +29,85 @@ function WelcomePage() {
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
+          <div className={styles.social_icons}>
+            <Tooltip title="Instagram" arrow={true} placement="top">
+              <IconButton style={{ marginLeft: "0px" }}>
+                <InstagramIcon
+                  style={{
+                    width: "40px",
+                    height: "40px",
+
+                    color: "#000000",
+                  }}
+                />
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Linkedin" arrow={true} placement="top">
+              <IconButton style={{ marginLeft: "0px" }}>
+                <LinkedInIcon
+                  style={{
+                    width: "40px",
+                    height: "40px",
+
+                    color: "#000000",
+                  }}
+                />
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Whatsapp" arrow={true} placement="top">
+              <IconButton style={{ marginLeft: "0px" }}>
+                <WhatsAppIcon
+                  style={{
+                    width: "40px",
+                    height: "40px",
+
+                    color: "#000000",
+                  }}
+                />
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Twitter" arrow={true} placement="top">
+              <IconButton style={{ marginLeft: "0px" }}>
+                <TwitterIcon
+                  style={{
+                    width: "40px",
+                    height: "40px",
+
+                    color: "#000000",
+                  }}
+                />
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Email" arrow={true} placement="top">
+              <IconButton style={{ marginLeft: "0px" }}>
+                <AlternateEmailIcon
+                  style={{
+                    width: "40px",
+                    height: "40px",
+
+                    color: "#000000",
+                  }}
+                />
+              </IconButton>
+            </Tooltip>
+
+            <Tooltip title="Github" arrow={true} placement="top">
+              <IconButton style={{ marginLeft: "0px" }}>
+                <GitHubIcon
+                  style={{
+                    width: "40px",
+                    height: "40px",
+
+                    color: "#000000",
+                  }}
+                />
+              </IconButton>
+            </Tooltip>
+          </div>
         </div>
 
         <div className={styles.intro_right_container}>
